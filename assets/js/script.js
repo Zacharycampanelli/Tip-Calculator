0ed62e7b36eb744a98ff59a6f2b8bca627e70269
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-04-07 11:06:24 -0400

    fixed bug so custom input tip works as soon as the user enters a number

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,47 +2,42 @@ let bill_amount;
 let bill_tip;
 let bill_number_of_people;
 
-const billAmount = document.querySelector('#amount')
+const billAmount = document.querySelector('#amount');
 const tipGroup = document.querySelector('.card_tip_btn_group');
 const btns = document.querySelectorAll('.btn');
 
 const numOfPeopleEl = document.querySelector('#number');
-
-const tipAmountEl = document.querySelector('#tip_amount')
-const totalAmountEl = document.querySelector('#total_amount')
-
+const customEl = document.querySelector('#custom');
+const tipAmountEl = document.querySelector('#tip_amount');
+const totalAmountEl = document.querySelector('#total_amount');
 
 const evaluateTipTotal = () => {
-  if(bill_amount && bill_tip && bill_number_of_people) {
-  let totalTipAmt = (bill_amount * bill_tip) / bill_number_of_people;
-  console.log(totalTipAmt);
-  let totalPerPerson = (bill_amount  * (1 + bill_tip) ) / bill_number_of_people;
-  tipAmountEl.textContent = totalTipAmt
-  console.log(totalTipAmt);
-  totalAmountEl.textContent = totalPerPerson;
-  console.log(totalPerPerson);
+  if (bill_amount && bill_tip && bill_number_of_people) {
+    let totalTipAmt = (bill_amount * bill_tip) / bill_number_of_people;
+    console.log(totalTipAmt);
+    let totalPerPerson = (bill_amount * (1 + bill_tip)) / bill_number_of_people;
+    tipAmountEl.textContent = totalTipAmt;
+    console.log(totalTipAmt);
+    totalAmountEl.textContent = totalPerPerson;
+    console.log(totalPerPerson);
   }
+};
 
-
-}
-
-
-billAmount.addEventListener("change", ()=> {
+billAmount.addEventListener('change', () => {
   bill_amount = billAmount.value;
-  bill_amount = parseFloat(bill_amount)
+  bill_amount = parseFloat(bill_amount);
   console.log(bill_amount);
   evaluateTipTotal();
-})
-numOfPeopleEl.addEventListener("change", ()=> {
+});
+numOfPeopleEl.addEventListener('change', () => {
   bill_number_of_people = numOfPeopleEl.value;
-  bill_number_of_people = parseFloat(bill_number_of_people)
+  bill_number_of_people = parseFloat(bill_number_of_people);
   console.log(bill_number_of_people);
   evaluateTipTotal();
-
-})
+});
 // tipGroup.addEventListener('click', ((e) => {
 //   // btns.filter(())
-  
+
 //   const selected = e.target;
 //   if(selected.hasAttribute('custom')) {
 //     bill_tip = selected.value / 100;
@@ -53,28 +48,32 @@ numOfPeopleEl.addEventListener("change", ()=> {
 //     // bill_tip = bill_tip.spl
 //     console.log(bill_tip);
 //   }
-  
+
 //   // selected.setAttribute('selected', true);
-  
+
 //   console.log(bill_tip);
 // }))
-tipGroup.addEventListener('click', ((e) => {
-  // btns.filter(())
-  
+tipGroup.addEventListener('click', (e) => {
   const selected = e.target;
-  if(selected.hasAttribute('custom')) {
-    bill_tip = selected.value / 100;
+  if (selected.hasAttribute('custom')) {
+    // bill_tip = selected.value / 100;
     console.log(bill_tip);
   } else {
     bill_tip = selected.value;
-    bill_tip = parseFloat(bill_tip)
+    bill_tip = parseFloat(bill_tip);
     // bill_tip = bill_tip.spl
     console.log(bill_tip);
-    
   }
   evaluateTipTotal();
-  
+
   // selected.setAttribute('selected', true);
-  
+
   console.log(bill_tip);
-}))
+});
+
+customEl.addEventListener('input', function()  {
+    bill_tip = this.value / 100;
+    console.log(bill_tip);
+  
+  evaluateTipTotal();
+});
