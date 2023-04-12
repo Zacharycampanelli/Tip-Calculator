28b9acead494a13b5b7dd38666e17e6bb217dd50
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-04-12 15:10:42 -0400

    reset button is disabled when there is no input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,17 @@
-let bill_amount;
-let bill_tip;
-let bill_number_of_people;
-
-const btns = document.querySelectorAll('.btn');
-
 const billAmountEl = document.querySelector('#amount');
 const numOfPeopleEl = document.querySelector('#number');
-const tipAmountEl = document.querySelector('#tip_amount');
-const tipGroup = document.querySelector('.card_tip_btn_group');
-const totalAmountEl = document.querySelector('#total_amount');
+const tipGroupEl = document.querySelector('.card_tip_btn_group');
+const btns = document.querySelectorAll('.btn');
 const customEl = document.querySelector('#custom');
+const tipAmountEl = document.querySelector('#tip_amount');
+const totalAmountEl = document.querySelector('#total_amount');
 const resetEl = document.querySelector('#reset_btn');
 const billErrorEl = document.querySelector('#bill_error');
 const peopleErrorEl = document.querySelector('#people_error');
 
-// const removeErrorMsg = (err)
+let bill_amount;
+let bill_tip;
+let bill_number_of_people;
 
 const addErrorMsg = (err) => {
   if (err.id === 'amount') {
@@ -22,6 +19,16 @@ const addErrorMsg = (err) => {
   }
   if (err.id === 'number') {
     peopleErrorEl.classList.remove('hidden');
+  }
+};
+
+const disableButton = () => {
+  if (bill_amount || bill_number_of_people || bill_tip) {
+    resetEl.classList.remove('disabled');
+    resetEl.disabled = false;
+  } else {
+    resetEl.disabled = true;
+    resetEl.classList.add('disabled');
   }
 };
 
@@ -38,23 +45,10 @@ const checkIfZero = (num, el, errMsgEl) => {
 };
 
 const evaluateTipTotal = () => {
-  if (bill_amount === undefined && bill_tip === undefined && bill_number_of_people === undefined) {
-    resetEl.disabled = true;
-    resetEl.classList.add('disabled');
-    console.log('disabled');
-  } else {
-    resetEl.classList.remove('disabled');
-    resetEl.disabled = false;
-    console.log('enabled');
-  }
-
   if (bill_amount > 0 && bill_tip > 0 && bill_number_of_people > 0) {
     let totalTipAmt = (bill_amount * bill_tip) / bill_number_of_people;
-    console.log(totalTipAmt);
-    tipAmountEl.textContent = totalTipAmt.toFixed(2);
-    console.log(totalTipAmt);
+    tipAmountEl.textContent = '$' + totalTipAmt.toFixed(2);
   }
-
   evaluateTotalPerPerson();
 };
 
@@ -62,15 +56,15 @@ const evaluateTotalPerPerson = () => {
   if (bill_amount > 0 && bill_tip > 0 && bill_number_of_people > 0) {
     let totalPerPerson = (bill_amount * (1 + bill_tip)) / bill_number_of_people;
     totalAmountEl.textContent = totalPerPerson.toFixed(2);
-    console.log(totalPerPerson);
   }
 };
 
 const getBillAmount = (e) => {
   bill_amount = billAmountEl.value;
   bill_amount = parseFloat(bill_amount);
+  disableButton();
   let check = checkIfZero(bill_amount, billAmountEl, billErrorEl);
-  if (!check) {
+  if (!check && bill_amount) {
     evaluateTipTotal();
   }
 };
@@ -78,10 +72,28 @@ const getBillAmount = (e) => {
 const getNumberOfPeople = (e) => {
   bill_number_of_people = numOfPeopleEl.value;
   bill_number_of_people = parseFloat(bill_number_of_people);
+  disableButton();
   let check = checkIfZero(bill_number_of_people, numOfPeopleEl, peopleErrorEl);
   if (!check) {
     evaluateTipTotal();
   }
+};
+
+const getTipAmount = (e) => {
+  const selected = e.target;
+  if (!selected.hasAttribute('custom')) {
+    bill_tip = selected.value;
+    bill_tip = parseFloat(bill_tip);
+    disableButton();
+    selectTipBox(selected);
+  }
+  evaluateTipTotal();
+};
+
+const getCustomTip = (e) => {
+  bill_tip = e.target.value / 100;
+  disableButton();
+  if (bill_tip) evaluateTipTotal();
 };
 
 const selectTipBox = (item) => {
@@ -98,36 +110,25 @@ const selectTipBox = (item) => {
   }
 };
 
-const getTipAmount = (e) => {
-  const selected = e.target;
-  if (!selected.hasAttribute('custom')) {
-    bill_tip = selected.value;
-    bill_tip = parseFloat(bill_tip);
-    selectTipBox(selected);
-  }
-  evaluateTipTotal();
-};
-
-const getCustomTip = (e) => {
-  bill_tip = e.target.value / 100;
-  console.log(bill_tip);
-  if (bill_tip) evaluateTipTotal();
-};
-
 const resetValues = (e) => {
   billAmountEl.value = '';
   numOfPeopleEl.value = '';
   customEl.value = '';
-  bill_amount = undefined;
-  bill_tip = undefined;
-  bill_number_of_people = undefined;
+  bill_amount = null;
+  bill_tip = null;
+  bill_number_of_people = null;
+  resetEl.disabled = true;
+  totalAmountEl.textContent = '$0.00';
+  tipAmountEl.textContent = '$0.00';
 
   let current = document.getElementById('active');
-  current.removeAttribute('id');
+  if (current) {
+    current.removeAttribute('id');
+  }
 };
 
-billAmountEl.addEventListener('change', getBillAmount);
-numOfPeopleEl.addEventListener('change', getNumberOfPeople);
-tipGroup.addEventListener('click', getTipAmount);
+billAmountEl.addEventListener('input', getBillAmount);
+numOfPeopleEl.addEventListener('input', getNumberOfPeople);
+tipGroupEl.addEventListener('click', getTipAmount);
 customEl.addEventListener('input', getCustomTip);
 resetEl.addEventListener('click', resetValues);
